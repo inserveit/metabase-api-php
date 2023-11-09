fa0baf091bec47e64c352f7ac66a596c05512f3e
Paul Radt <paul@inserve.nl>
2023-11-09 09:35:28 +0100

    Add aggregation to query

--- src/Model/Card/Query.php
+++ src/Model/Card/Query.php
@@ -8,13 +8,21 @@ namespace Inserve\MetabaseAPI\Model\Card;
 class Query
 {
     protected ?int $sourceTable = null;
-
+    protected ?array $aggregation = null;
     /**
      * @return int|null
      */
     public function getSourceTable(): ?int
     {
         return $this->sourceTable;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getAggregation(): ?array
+    {
+        return $this->aggregation;
     }
 
     /**
@@ -25,6 +33,18 @@ class Query
     public function setSourceTable(?int $sourceTable): self
     {
         $this->sourceTable = $sourceTable;
+
+        return $this;
+    }
+
+    /**
+     * @param array|null $aggregation
+     *
+     * @return self
+     */
+    public function setAggregation(?array $aggregation): self
+    {
+        $this->aggregation = $aggregation;
 
         return $this;
     }
