6884ad5c781f7b1ac948c7b7bcde41514cc624f7
Paul Radt <paul@inserve.nl>
2024-09-30 16:45:11 +0200

    Try to fix PHPStan error

--- src/API/EmbedAPI.php
+++ src/API/EmbedAPI.php
@@ -39,7 +39,7 @@ class EmbedAPI extends AbstractAPIClient
 
         if ($seconds) {
             $expiresAt = (new DateTimeImmutable())->modify(sprintf('+%d seconds', $seconds));
-            if ($expiresAt === false) {
+            if (!$expiresAt instanceof DateTimeImmutable) {
                 $expiresAt = null;
             }
         }
